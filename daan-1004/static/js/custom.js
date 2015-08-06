94a9f93558d5d74e19c1d12dc6ae7bbcf1331137
stretchhog <stretchhog@github.com>
2015-08-06 20:24:17 +0200

    More interesting gallery for paintings

--- daan-1004/static/js/custom.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-    $('.panel-image img.panel-image-preview').on('click', function(e) {
-        $(this).closest('.panel-image').toggleClass('hide-panel-body');
-    });
-});/**
- * Created by Stretchhog on 2015-07-16.
- */
